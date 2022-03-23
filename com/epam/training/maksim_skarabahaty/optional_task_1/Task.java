d8c41dbd646a1033e5441729d851ff6b2086dcd4
Maksim Skarabahaty <skaregok@gmail.com>
2022-03-23 18:10:03 +0300

    Optional task 1 point 5 undone

--- com/epam/training/maksim_skarabahaty/optional_task_1/Task.java
+++ com/epam/training/maksim_skarabahaty/optional_task_1/Task.java
@@ -6,36 +6,50 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task {
 
-    /*
-
-4.     Найти число, в котором количество различных цифр минимально. Если таких чисел несколько, найти первое из них.
-
-5.     Найти количество чисел, содержащих только четные цифры, а среди оставшихся — количество чисел с равным числом четных и нечетных цифр.
-
-6.     Найти число, цифры в котором идут в строгом порядке возрастания. Если таких чисел несколько, найти первое из них.
-
-7.     Найти число, состоящее только из различных цифр. Если таких чисел несколько, найти первое из них.*/
 
     public static void main(String[] args) {
 
-        String[] numbers = readValuesFromConsole();
-
-        findLongestNumber(numbers);
-        findShortestNumber(numbers); //task 1 end
-        delimit();
-
-        List<String> numbersList = Arrays.asList(numbers);
-
-        printInIncreasingOrder(numbersList);
-        printInDecreasingOrder(numbersList);
-        delimit();//task 2 end
-
-        calculateAverageAndPrintNumbersRelativeToAverage(numbers);//task 3 end
+        String[] numbersInString = readValuesFromConsole();
 
 
+        findLongestNumber(numbersInString);
+        findShortestNumber(numbersInString);
+        delimiter();//task 1 end
+
+
+        printInIncreasingOrder(numbersInString);
+        printInDecreasingOrder(numbersInString);
+        delimiter();//task 2 end
+
+
+        double averageLength = getAverageLength(numbersInString);
+        printNumbersRelativeToAverage(numbersInString, averageLength);
+        delimiter();//task 3 end
+
+
+        checkForCondition4(numbersInString);
+        delimiter();//task 4 end
+
+        /*5.Найти количество чисел, содержащих только четные цифры,
+        а среди оставшихся — количество чисел с равным числом четных и нечетных цифр.
+         */
+        checkForCondition5(numbersInString);
+
+
+
+
+    /*
+
+6. Найти число, цифры в котором идут в строгом порядке возрастания.
+ Если таких чисел несколько, найти первое из них.
+
+7. Найти число, состоящее только из различных цифр.
+ Если таких чисел несколько, найти первое из них.*/
     }
 
     private static String[] readValuesFromConsole() {
@@ -86,22 +100,25 @@ public class Task {
         System.out.println("Shortest number = " + shortestElement + " , length = " + minLength);
     }
 
-    private static void delimit() {
+    private static void delimiter() {
         System.out.println("-".repeat(50));
     }
 
-    private static void printInIncreasingOrder(List<String> numbersList) {
-        numbersList.sort(Comparator.comparingInt(String::length));
-        System.out.println("Ordered incr. : " + numbersList);
+    private static void printInIncreasingOrder(String[] numbersInString) {
+        String[] localString = Arrays.copyOf(numbersInString, numbersInString.length);
+        List<String> strings = Arrays.asList(localString);
+        strings.sort(Comparator.comparingInt(String::length));
+        System.out.println("Ordered incr. : " + strings);
     }
 
-    private static void printInDecreasingOrder(List<String> numbersList) {
-        numbersList.sort((leftString, rightString) -> rightString.length() - leftString.length());
-        System.out.println("Ordered decr. : " + numbersList);
+    private static void printInDecreasingOrder(String[] numbersInString) {
+        String[] localString = Arrays.copyOf(numbersInString, numbersInString.length);
+        List<String> strings = Arrays.asList(localString);
+        strings.sort((leftString, rightString) -> rightString.length() - leftString.length());
+        System.out.println("Ordered decr. : " + strings);
     }
 
-    private static void calculateAverageAndPrintNumbersRelativeToAverage(String[] numbers) {
-
+    private static double getAverageLength(String[] numbers) {
         int lengthsSum = 0;
         for (String number : numbers) {
             lengthsSum += number.length();
@@ -109,7 +126,11 @@ public class Task {
 
         double averageLength = (double)lengthsSum / numbers.length;
         System.out.println("Average length = " + averageLength);
-        delimit();
+        delimiter();
+        return averageLength;
+    }
+
+    private static void printNumbersRelativeToAverage(String[] numbers, double averageLength) {
 
         System.out.println("Numbers, longer than average: ");
         for (String number : numbers) {
@@ -117,13 +138,78 @@ public class Task {
                 System.out.println(number + ", length = " + number.length());
             }
         }
-        delimit();
+        delimiter();
         System.out.println("Numbers, shorter than average: ");
         for (String number : numbers) {
             if (number.length() < averageLength) {
                 System.out.println(number + ", length = " + number.length());
             }
         }
-        delimit();
+    }
+
+    private static void checkForCondition4(String[] numbersInString) {
+
+        int minDigitsCounter = Integer.MAX_VALUE;
+        String numberWithLeastDifferentDigits = "";
+
+        for (String numberInString : numbersInString) {
+
+            String digits = "[0123456789]";
+            Pattern pattern = Pattern.compile(digits);
+            Matcher matcher = pattern.matcher(numberInString);
+            int localCounter = 0;
+
+            while (matcher.find()) {
+                String group = matcher.group();
+                localCounter++;
+                digits = digits.replace(group, "");
+
+                pattern = Pattern.compile(digits);
+                matcher = pattern.matcher(numberInString);
+            }
+
+            if (localCounter < minDigitsCounter) {
+                minDigitsCounter = localCounter;
+                numberWithLeastDifferentDigits = numberInString;
+            }
+        }
+
+        System.out.println("Number with least different numbers: " + numberWithLeastDifferentDigits);
+    }
+
+    private static void checkForCondition5(String[] numbersInString) {
+        /*5.Найти количество чисел, содержащих только четные цифры,
+        а среди оставшихся — количество чисел с равным числом четных и нечетных цифр.
+         */
+
+        String[] localArray = Arrays.copyOf(numbersInString, numbersInString.length);
+        List<String> localArrayList = Arrays.asList(localArray);
+
+        int localCounter = 0;
+
+        for (int i = 0; i < localArray.length; i++) {
+            String[] digitsOfNumber = localArray[i].split("");
+            boolean areAllEven = true;
+            for (String digitOfNumber : digitsOfNumber) {
+
+                int number = Integer.parseInt(digitOfNumber);
+                if (number % 2 != 0) {
+                    areAllEven = false;
+                    break;
+                }
+            }
+
+            if (areAllEven) {
+                localCounter++;
+            }
+        }
+        for (String numberInString : localArray) {
+
+
+        }
+
+
+        System.out.println("Amount of number with all even digits = " + localCounter);
+
     }
 }
